f28972cac0069293f2d5ab79cbdf408b76d308e5
Monica Gerard <mlgvla@gmail.com>
2022-08-15 21:16:55 -0400

    solved

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.any?{ |freebie| freebie.item_name.downcase == item_name.downcase }
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev_id: dev.id) unless freebie.dev != self
+    end
 end
--- db/migrate/20210721161602_create_devs.rb
+++ db/migrate/20210721161602_create_devs.rb
@@ -2,6 +2,8 @@ class CreateDevs < ActiveRecord::Migration[6.1]
   def change
     create_table :devs do |t|
       t.string :name
+
+      t.timestamps
     end
   end
 end
